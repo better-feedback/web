f8f1bf316744a6a447a313c7fc6f5f82500d64c3
Dong-Ha Kim <dongha.kim210@gmail.com>
2021-12-11 14:37:16 +0100

    Use env vars for configuration of app

--- utils/config.ts
+++ utils/config.ts
@@ -1,73 +1,37 @@
-export const getContractName = (): string => {
-  const env = getEnv()
-  return env === 'testnet' ? 'better.betterhq.testnet' : 'better.better.near'
+export function getContractName() {
+  return getNearConfig().contractName
 }
 
-export const getEnv = (): string => {
-  if (typeof window !== 'undefined') {
-    const href = window.location.href
-    return /-testnet/.test(href) || /localhost/.test(href)
-      ? 'testnet'
-      : 'mainnet'
+export function getNearConfig() {
+  return {
+    networkId: process.env.NEXT_PUBLIC_NEAR_NETWORK_ID || 'testnet',
+    nodeUrl:
+      process.env.NEXT_PUBLIC_NEAR_NODE_URL || 'https://rpc.testnet.near.org',
+    contractName:
+      process.env.NEXT_PUBLIC_NEAR_CONTRACT_NAME || 'better.betterhq.testnet',
+    walletUrl:
+      process.env.NEXT_PUBLIC_NEAR_WALLET_URL ||
+      'https://wallet.testnet.near.org',
+    helperUrl:
+      process.env.NEXT_PUBLIC_NEAR_HELPER_URL ||
+      'https://helper.testnet.near.org',
   }
-  return 'mainnet'
+}
+
+export function getGitHubConfig() {
+  return {
+    repoOwner: process.env.NEXT_PUBLIC_REPO_OWNER || '',
+    repoName: process.env.NEXT_PUBLIC_REPO_NAME || '',
+    betterIssueLabel: process.env.NEXT_PUBLIC_REPO_ISSUE_LABEL || 'better',
+    pat: process.env.REPO_PAT,
+  }
 }
 
 export function getConfig() {
-  const env = getEnv()
-  switch (env) {
-    case 'production':
-    case 'mainnet':
-      return {
-        networkId: 'mainnet',
-        nodeUrl: 'https://rpc.mainnet.near.org',
-        contractName: getContractName(),
-        walletUrl: 'https://wallet.near.org',
-        helperUrl: 'https://helper.mainnet.near.org',
-      }
-    case 'development':
-    case 'testnet':
-      return {
-        networkId: 'testnet',
-        nodeUrl: 'https://rpc.testnet.near.org',
-        contractName: getContractName(),
-        walletUrl: 'https://wallet.testnet.near.org',
-        helperUrl: 'https://helper.testnet.near.org',
-      }
-    case 'betanet':
-      return {
-        networkId: 'betanet',
-        nodeUrl: 'https://rpc.betanet.near.org',
-        contractName: getContractName(),
-        walletUrl: 'https://wallet.betanet.near.org',
-        helperUrl: 'https://helper.betanet.near.org',
-      }
-    case 'local':
-      return {
-        networkId: 'local',
-        nodeUrl: 'http://localhost:3030',
-        keyPath: `${process.env.HOME}/.near/validator_key.json`,
-        walletUrl: 'http://localhost:4000/wallet',
-        contractName: getContractName(),
-      }
-    case 'test':
-    case 'ci':
-      return {
-        networkId: 'shared-test',
-        nodeUrl: 'https://rpc.ci-testnet.near.org',
-        contractName: getContractName(),
-        masterAccount: 'test.near',
-      }
-    case 'ci-betanet':
-      return {
-        networkId: 'shared-test-staging',
-        nodeUrl: 'https://rpc.ci-betanet.near.org',
-        contractName: getContractName(),
-        masterAccount: 'test.near',
-      }
-    default:
-      throw Error(
-        `Unconfigured environment '${env}'. Can be configured in src/config.js.`
-      )
+  return {
+    github: getGitHubConfig(),
+    chains: {
+      near: getNearConfig(),
+    },
   }
 }
